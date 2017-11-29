8bfec297bfdf41df91b70ef68e7b44061c7f6a38
Remy Sharp <remy@remysharp.com>
2017-11-29 22:41:03 +0000

    feat: support class arrow function binding

--- next.js
+++ next.js
@@ -5,6 +5,7 @@ module.exports = {
     commonjs: true,
   },
   extends: 'eslint:recommended',
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
